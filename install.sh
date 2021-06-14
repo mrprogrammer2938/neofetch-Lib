6efaea961fcf74ac833d06cd1b1f0098e33d6e95
Mr.programmer <78996423+mrprogrammer2938@users.noreply.github.com>
2021-06-14 02:53:03 -0400

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# This code write by Mr.nope
+clear
+echo "Installing..."
+sleep 2
+chmod a+x neofetch.pyc
+sudo cp neofetch.pyc /usr/lib/python3.6
+echo ""
+echo "Installing..., Finish...!"
+echo ""
+exit 1
---
+++ uninstall.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# This code write by Mr.nope
+clear
+echo "Uninstalling..."
+sleep 2
+cd /usr/lib/python3.6 && sudo rm neofeth.pyc
+echo ""
+echo "Uninstalling..., Finish...!"
+echo ""
+exit 1
